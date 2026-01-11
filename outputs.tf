0f413f77035562c5247e9a7089215f6ebe6ea4f3
Daniel Allen <dan@aeternagroup.net>
2026-01-11 00:45:00 +0000

    Update to schema version v4.57.0

---
+++ outputs.tf
@@ -0,0 +1,5 @@
+output "api_management_api_operation_tags" {
+  description = "All api_management_api_operation_tag resources"
+  value       = azurerm_api_management_api_operation_tag.api_management_api_operation_tags
+}
+
