24f9092d37be69c3c98c4bfbd4575c0091288593
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:15:21 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,13 @@
-output "api_management_api_operation_tags" {
-  description = "All api_management_api_operation_tag resources"
-  value       = azurerm_api_management_api_operation_tag.api_management_api_operation_tags
-}
 output "api_management_api_operation_tags_api_operation_id" {
-  description = "List of api_operation_id values across all api_management_api_operation_tags"
-  value       = [for k, v in azurerm_api_management_api_operation_tag.api_management_api_operation_tags : v.api_operation_id]
+  description = "Map of api_operation_id values across all api_management_api_operation_tags, keyed the same as var.api_management_api_operation_tags"
+  value       = { for k, v in azurerm_api_management_api_operation_tag.api_management_api_operation_tags : k => v.api_operation_id }
 }
 output "api_management_api_operation_tags_display_name" {
-  description = "List of display_name values across all api_management_api_operation_tags"
-  value       = [for k, v in azurerm_api_management_api_operation_tag.api_management_api_operation_tags : v.display_name]
+  description = "Map of display_name values across all api_management_api_operation_tags, keyed the same as var.api_management_api_operation_tags"
+  value       = { for k, v in azurerm_api_management_api_operation_tag.api_management_api_operation_tags : k => v.display_name }
 }
 output "api_management_api_operation_tags_name" {
-  description = "List of name values across all api_management_api_operation_tags"
-  value       = [for k, v in azurerm_api_management_api_operation_tag.api_management_api_operation_tags : v.name]
+  description = "Map of name values across all api_management_api_operation_tags, keyed the same as var.api_management_api_operation_tags"
+  value       = { for k, v in azurerm_api_management_api_operation_tag.api_management_api_operation_tags : k => v.name }
 }
 
